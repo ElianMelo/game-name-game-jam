2941a8def0330b2c77a303a19c9715e8ebfeb45e
Elian Melo <elianmelomorais@gmail.com>
2026-04-02 18:05:48 -0300

    Implemented enemy spawner

---
+++ Assets/_Project/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private List<Transform> spawnPoints = new();
+
+    private float currentSpawnTime = 0;
+
+    private void Start()
+    {
+        currentSpawnTime = spawnTime;
+    }
+
+    void Update()
+    {
+        SpawnUnit();
+    }
+
+    private void SpawnUnit()
+    {
+        currentSpawnTime -= Time.deltaTime;
+        if (currentSpawnTime > 0) return;
+        currentSpawnTime = spawnTime;
+        GameObject enemyObj = Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Count)].position, Quaternion.identity);
+    }
+}
